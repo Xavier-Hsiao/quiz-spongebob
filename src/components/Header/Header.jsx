9ea1ad42693a4a344dc2b392e98f3c05a4556f6a
Xavier-Hsiao <tim88776@gmail.com>
2024-01-26 19:43:53 +0800

    chore: change logo url

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,8 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <img
-        src="src\assets\Spongebob_Squarepants-logo-FAC95E82E1-seeklogo.com.png"
+        src="https://raw.githubusercontent.com/Xavier-Hsiao/quiz-spongebob/master/src/assets/Spongebob_Squarepants-logo-FAC95E82E1-seeklogo.com.png
+"
         alt="spongebob-logo"
       />
       <h1>海綿寶寶測驗</h1>
